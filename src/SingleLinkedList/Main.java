2c21a0982656f5fa7f4c739236a92c550d8e5f08
shanmukhmaddimsetti <shanmukhmaddimsetti@gmail.com>
2024-07-13 11:35:55 +0530

    first commit

---
+++ src/SingleLinkedList/Main.java
@@ -0,0 +1,7 @@
+package SingleLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/SingleLinkedList/Node.java
@@ -0,0 +1,14 @@
+package SingleLinkedList;
+
+public class Node {
+    int data;
+    Node next;
+
+    Node(){
+
+    }
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+}
