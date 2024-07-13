421ef47e21815f71098ae4e6c5508f53c3f3e0c1
shanmukhmaddimsetti <shanmukhmaddimsetti@gmail.com>
2024-07-13 14:28:33 +0530

    Single Linked List

--- src/SingleLinkedList/LinkedList.java
+++ src/SingleLinkedList/LinkedList.java
@@ -4,7 +4,7 @@ public class LinkedList {
     Node head = null;
     Node tail = null;
 
-//    Normal Approach O(n)
+//    Normal Approach O(n^2)
     void addNode(int data){
 
         if(head == null){
@@ -20,7 +20,7 @@ public class LinkedList {
 
     }
 
-//    Optimized Appraoch O(1)
+//    Optimized Approach O(1)
 
     void insertNode(int data){
         Node newNode = new Node(data);
@@ -34,12 +34,65 @@ public class LinkedList {
 
     }
 
+    void insertAtHead(int data){
+        Node newNode  = new Node(data);
+        newNode.next = head;
+        head = newNode;
+        return;
+    }
+    void insertAtPosition(int data, int pos){
+        if (pos == 1){
+            insertAtHead(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        int count = 1;
+        Node temp = head;
+
+        while(count != (pos - 1)){
+            count++;
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    void deleteHead(){
+        if (head == null){
+            return;
+        }
+        head = head.next;
+        return;
+    }
+
+    void deleteAtPosition(int pos){
+        if (head == null){
+            return;
+        }
+        if(pos <= 0 || pos > lengthOfLinkedList()){
+            return;
+        }
+        if (pos == 1){
+            head = head.next;
+            return;
+        }
+        int count = 1;
+        Node temp = head;
+
+        while(temp != null && count < (pos - 1)){
+            count++;
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+    }
+
     void printLinkedList(){
         Node temp = head;
         while (temp != null){
             System.out.print(temp.data + " -> ");
             temp = temp.next;
         }
+        System.out.print(temp);
         System.out.println();
     }
 
@@ -51,5 +104,21 @@ public class LinkedList {
             temp = temp.next;
         }
         return count;
+    }
+
+    void incrementNodeValue(){
+        Node temp = head;
+        while (temp != null){
+            temp.data++;
+            temp = temp.next;
+        }
+    }
+
+    void decrementNodeValue(){
+        Node temp = head;
+        while (temp != null){
+            temp.data--;
+            temp = temp.next;
+        }
     }
 }
--- src/SingleLinkedList/Main.java
+++ src/SingleLinkedList/Main.java
@@ -2,17 +2,15 @@ package SingleLinkedList;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-//        list.insertNode(1);
-//        list.insertNode(2);
-//        list.insertNode(3);
-//        list.printLinkedList();
-
         Scanner sc = new Scanner(System.in);
+        LinkedList list = new LinkedList();
+
         while (true){
             System.out.println("1. Insert Into Single Linked List: ");
             System.out.println("2. Length of the Single Linked List: ");
             System.out.println("3. Print the Single Linked List: ");
+            System.out.println("4. Insert at specific position: ");
+            System.out.println("5. Delete at position: ");
             System.out.println("Enter your choice: ");
             int ch = sc.nextInt();
             switch (ch){
@@ -28,6 +26,19 @@ public class Main {
                 case 3:
                     System.out.println("Printing the Linked List: ");
                     list.printLinkedList();
+                    break;
+                case 4:
+                    System.out.println("Inserting at Specific Position of Linked List: ");
+                    System.out.println("Enter data to insert: ");
+                    int ele = sc.nextInt();
+                    System.out.println("Enter the position to insert: ");
+                    int pos = sc.nextInt();
+                    list.insertAtPosition(ele, pos);
+                    break;
+                case 5:
+                    System.out.println("Deleting at position: ");
+                    int position = sc.nextInt();
+                    list.deleteAtPosition(position);
                     break;
                 default:
                     System.exit(0);
